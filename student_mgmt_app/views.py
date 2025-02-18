6ab4840e8971efe2f71fee13f8853a86bb524019
nirajan-sanjyal <sanjyalnirajan6@gmail.com>
2025-02-18 17:56:26 +0545

    commit

--- student_mgmt_app/views.py
+++ student_mgmt_app/views.py
@@ -50,59 +50,61 @@ def registration(request):
     
 
 def doRegistration(request):
-    first_name = request.GET.get('first_name')
-    last_name = request.GET.get('last_name')
-    email_id = request.GET.get('email')
-    password = request.GET.get('password')
-    confirm_password = request.GET.get('confirmPassword')
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        email_id = request.POST.get('email')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirmPassword')
+        breakpoint()
+        print(email_id)
+        print(password)
+        print(confirm_password)
+        print(first_name)
+        print(last_name)
+        if not (email_id and password and confirm_password):
+            messages.error(request, 'Please provide all the details!!')
+            return render(request, 'registration.html')
+        
+        if password != confirm_password:
+            messages.error(request, 'Both passwords should match!!')
+            return render(request, 'registration.html')
 
-    print(email_id)
-    print(password)
-    print(confirm_password)
-    print(first_name)
-    print(last_name)
-    if not (email_id and password and confirm_password):
-        messages.error(request, 'Please provide all the details!!')
+        is_user_exists = CustomUser.objects.filter(email=email_id).exists()
+
+        if is_user_exists:
+            messages.error(request, 'User with this email id already exists. Please proceed to login!!')
+            return render(request, 'registration.html')
+
+        user_type = get_user_type_from_email(email_id)
+
+        if user_type is None:
+            messages.error(request, "Please use valid format for the email id: '<username>.<staff|student|hod>@<college_domain>'")
+            return render(request, 'registration.html')
+
+        username = email_id.split('@')[0].split('.')[0]
+
+        if CustomUser.objects.filter(username=username).exists():
+            messages.error(request, 'User with this username already exists. Please use different username')
+            return render(request, 'registration.html')
+
+        user = CustomUser()
+        user.username = username
+        user.email = email_id
+        user.password = password
+        user.user_type = user_type
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+        
+        if user_type == CustomUser.STAFF:
+            Staffs.objects.create(admin=user)
+        elif user_type == CustomUser.STUDENT:
+            Students.objects.create(admin=user)
+        elif user_type == CustomUser.HOD:
+            AdminHOD.objects.create(admin=user)
         return render(request, 'registration.html')
-    
-    if password != confirm_password:
-        messages.error(request, 'Both passwords should match!!')
-        return render(request, 'registration.html')
-
-    is_user_exists = CustomUser.objects.filter(email=email_id).exists()
-
-    if is_user_exists:
-        messages.error(request, 'User with this email id already exists. Please proceed to login!!')
-        return render(request, 'registration.html')
-
-    user_type = get_user_type_from_email(email_id)
-
-    if user_type is None:
-        messages.error(request, "Please use valid format for the email id: '<username>.<staff|student|hod>@<college_domain>'")
-        return render(request, 'registration.html')
-
-    username = email_id.split('@')[0].split('.')[0]
-
-    if CustomUser.objects.filter(username=username).exists():
-        messages.error(request, 'User with this username already exists. Please use different username')
-        return render(request, 'registration.html')
-
-    user = CustomUser()
-    user.username = username
-    user.email = email_id
-    user.password = password
-    user.user_type = user_type
-    user.first_name = first_name
-    user.last_name = last_name
-    user.save()
-    
-    if user_type == CustomUser.STAFF:
-        Staffs.objects.create(admin=user)
-    elif user_type == CustomUser.STUDENT:
-        Students.objects.create(admin=user)
-    elif user_type == CustomUser.HOD:
-        AdminHOD.objects.create(admin=user)
-    return render(request, 'login_page.html')
+    return render(request, 'registration.html')
 
     
 def logout_user(request):
